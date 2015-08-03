35cf4d6bac5c63e27f52fbac5e123480d61d4271
Gina && Ryan <gina+ryan@appacademy.io>
2015-08-03 14:20:03 -0400

    Add Minesweeper#valid?.

--- minesweeper.rb
+++ minesweeper.rb
@@ -14,6 +14,18 @@ class Minesweeper
 
   end
 
+  def valid?(action, pos)
+    return false if !on_board?(pos) &&
+    if action == "R"
+      @board[pos].revealed == false &&
+      @board[pos].flagged == false
+    elsif action == "F"
+      @board[pos].revealed == false
+    else
+      false
+    end
+  end
+
   def on_board?(pos)
     pos.all? { |coord| coord.between?(0, board.length - 1) }
   end
