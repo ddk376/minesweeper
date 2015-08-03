41e5bcaaeb525c003f1bcbe70d8d82fc4e88ddf9
Gina && Ryan <gina+ryan@appacademy.io>
2015-08-03 13:52:34 -0400

    Add Board#populate

--- board.rb
+++ board.rb
@@ -8,6 +8,19 @@ class Board
     @bombs = bombs
   end
 
+  def populate
+    bomb_pos = bomb_positions
+    (0..grid.size - 1) do |y|
+      (0..grid[0].size - 1) do |x|
+        if bomb_pos.include?([x,y])
+          self[[x,y]] = Tile.new([x,y], true, self)
+        else
+          self[[x,y]] = Tile.new([x,y], false, self)
+        end
+      end
+    end
+  end
+
   def bomb_positions
     bomb_positions = []
     bombs.times do
