5b9cf5790f7a475b1b42edc2a8258d731075e7d7
Gina && Ryan <gina+ryan@appacademy.io>
2015-08-03 11:53:07 -0400

    Initialize Tile class with @bomb, @board, @flagged, and @revealed instance variables.

--- tile.rb
+++ tile.rb
@@ -1,4 +1,13 @@
 require 'colorize'
 
 class Tile
+  attr_accessor :flagged, :revealed
+  attr_reader :bomb, :board
+
+  def initialize(bomb, board)
+    @bomb = bomb
+    @board = board
+    @flagged = false
+    @revealed = false
+  end
 end
