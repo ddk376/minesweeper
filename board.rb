d711d1ebc588b4e1bd193448864e7b1e71766868
Gina && Ryan <gina+ryan@appacademy.io>
2015-08-03 13:49:03 -0400

    Add Board#bomb_positions.

--- board.rb
+++ board.rb
@@ -1,10 +1,19 @@
 require_relative 'tile'
 
 class Board
-  attr_reader :size, :bombs
+  attr_reader :size, :bombs, :grid
 
   def intialize(size = 9, bombs = 10)
     @grid = Array.new(size) { Array.new(size) }
     @bombs = bombs
+  end
+
+  def bomb_positions
+    bomb_positions = []
+    bombs.times do
+      rand_pos = [rand(grid.size), rand(grid.size)]
+      bomb_positions << rand_pos if !bomb_positions.include?(rand_pos)
+    end
+    bomb_positions
   end
 end
