f26eccddc86b645e862470f1ae9eb393922ba476
Gina && Ryan <gina+ryan@appacademy.io>
2015-08-03 13:45:20 -0400

    Initialize Board class.

--- board.rb
+++ board.rb
@@ -1,4 +1,10 @@
 require_relative 'tile'
 
 class Board
+  attr_reader :size, :bombs
+
+  def intialize(size = 9, bombs = 10)
+    @grid = Array.new(size) { Array.new(size) }
+    @bombs = bombs
+  end
 end
